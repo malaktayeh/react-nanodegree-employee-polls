64ed4ecb0c7ac233e8430d994e66d40719356567
tenshi <malaktayeh@outlook.com>
2022-08-04 20:22:38 -0400

    basic login page

---
+++ employee-polls/src/app/components/Sidebar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Sidebar() {
+  return <div id="sidebar">Sidebar</div>;
+}
+
+export default Sidebar;
--- employee-polls/src/app/views/Login.js
+++ employee-polls/src/app/views/Login.js
@@ -1,29 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Sidebar from '../components/Sidebar';
 
 function Login() {
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We will never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+  const [validated, setValidated] = useState(false);
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <Container fluid>
+      <Row className="justify-content-md-center">
+        {/* Sidebar */}
+        <Col sm={0} lg={2} className="bg-light">
+          <div className="d-none d-lg-block">
+            <Sidebar />
+          </div>
+        </Col>
+
+        {/* Login Form */}
+        <Col sm={12} lg={10}>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter your username.
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control required type="password" placeholder="Password" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+
+            <Button type="submit">Submit</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
