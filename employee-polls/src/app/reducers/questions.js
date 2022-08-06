dda7780d77a2c277ba88d80809a268764c28abbb
tenshi <malaktayeh@outlook.com>
2022-08-06 13:29:04 -0400

    add redux-thunk middleware dependencies

--- employee-polls/src/app/reducers/questions.js
+++ employee-polls/src/app/reducers/questions.js
@@ -1,6 +1,6 @@
 import { GET_QUESTIONS } from '../actions/questions';
 
-export default function questions(state = {}, action) {
+export default function questions(action, state = {}) {
   switch (action.type) {
     case GET_QUESTIONS:
       return {
--- employee-polls/src/app/reducers/users.js
+++ employee-polls/src/app/reducers/users.js
@@ -1,6 +1,6 @@
 import { GET_USERS } from '../actions/users';
 
-export default function users(state = {}, action) {
+export default function users(action, state = {}) {
   switch (action.type) {
     case GET_USERS:
       return {
