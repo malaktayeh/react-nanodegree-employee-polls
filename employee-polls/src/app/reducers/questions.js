ce8876d83ee58a5b35bdcdbb091fc35da6f29762
tenshi <malaktayeh@outlook.com>
2022-08-06 13:21:15 -0400

    change reducers being in wrong folder

--- employee-polls/src/app/reducers/questions.js
+++ employee-polls/src/app/reducers/questions.js
@@ -1,4 +1,4 @@
-import { GET_QUESTIONS } from '../app/actions/questions';
+import { GET_QUESTIONS } from '../actions/questions';
 
 export default function questions(state = {}, action) {
   switch (action.type) {
