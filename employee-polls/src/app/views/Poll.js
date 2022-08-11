672a60c1a0e186a33e2bfa15039ec917b776196d
tenshi <malaktayeh@outlook.com>
2022-08-11 19:46:30 -0400

    ui of poll page

--- employee-polls/src/app/views/Poll.js
+++ employee-polls/src/app/views/Poll.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { useLocation } from 'react-router-dom';
+import Button from 'react-bootstrap/esm/Button';
 import NavBar from '../components/Navbar';
 
 function Poll() {
@@ -10,10 +13,20 @@ function Poll() {
     <>
       <NavBar />
       <Container>
-        <h2 className="my-5 mx-2">Poll page</h2>
-        <h4>Would you rather...?</h4>
-        <p>{state.q.optionOne.text}</p>
-        <p>{state.q.optionTwo.text}</p>
+        <h2 className="my-5 mx-2 justify-content-center">Poll page</h2>
+        <Row className="my-5">
+          <Col xs>
+            <h4 className="justify-content-center">Would you rather...?</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs>
+            <Button>{state.q.optionOne.text}</Button>
+          </Col>
+          <Col xs>
+            <Button>{state.q.optionTwo.text}</Button>
+          </Col>
+        </Row>
       </Container>
     </>
   );
