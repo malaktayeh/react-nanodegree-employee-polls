2f1ea9cb01ffc975cc836ac5a57a7a0bad05c3c7
tenshi <malaktayeh@outlook.com>
2022-08-21 06:39:32 -0400

    add voting statistics to user on poll page

--- employee-polls/src/app/views/Poll.js
+++ employee-polls/src/app/views/Poll.js
@@ -1,17 +1,26 @@
+/* eslint-disable no-console */
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useLocation, Link } from 'react-router-dom';
 import Button from 'react-bootstrap/esm/Button';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import NavBar from '../components/Navbar';
 import { vote } from '../features/questionsSlice';
+import { selectById } from '../features/usersSlice';
 
 function Poll() {
   const dispatch = useDispatch();
   const { state } = useLocation();
+  const author = useSelector((s) => selectById(s, state.q.author));
+  const date = new Date(state.q.timestamp);
+
+  const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
   const [voted, setVoted] = useState(state.voteStatus);
+  const [selected, setSelected] = useState();
+
   const [votedForOptionOne, setVotedForOptionOne] = useState(
     state.q.optionOne.votes.includes(state.authedUserId)
   );
@@ -19,12 +28,20 @@ function Poll() {
     state.q.optionTwo.votes.includes(state.authedUserId)
   );
 
+  const optionOneVote = state.q.optionOne.votes;
+  const optionTwoVote = state.q.optionTwo.votes;
+  const totalVotes = optionOneVote.length + optionTwoVote.length;
+  // eslint-disable-next-line no-console
+  console.log(totalVotes);
+
   const handleClick = (selection) => {
     if (selection === 'optionOne') {
       setVotedForOptionOne((prevAnswer) => !prevAnswer);
+      setSelected(selection);
     }
     if (selection === 'optionTwo') {
       setVotedForOptionTwo((prevAnswer) => !prevAnswer);
+      setSelected(selection);
     }
 
     const submission = {
@@ -42,8 +59,18 @@ function Poll() {
       <Container style={{ maxWidth: '750px' }}>
         <h2 className="my-5 mx-2">Poll page</h2>
         <Row className="my-5">
-          <Col xs>
+          <Col>
             <h4>Would you rather...?</h4>
+            <div style={{ display: 'flex', alignItems: 'end', justifyContent: 'end' }}>
+              <p className="mb-0">
+                Posted by {author.name} on {formattedDate}
+              </p>
+              <img
+                src={`${author.avatarURL}`}
+                alt="User Icon"
+                style={{ height: '100px', paddingLeft: '25px' }}
+              />
+            </div>
           </Col>
         </Row>
         {/* Unanswered Poll question UI */}
@@ -109,7 +136,17 @@ function Poll() {
           ) : (
             <>
               <p>Poll answered!</p>
-              <Button variant="light">
+              <p>
+                You voted for the following: <b>{state.q[selected].text}</b>
+              </p>
+              <p>
+                <b>{totalVotes}</b> colleagues selected the same option. Percentage of people who
+                voted like you:{' '}
+                <b>
+                  {Math.round(100 * (state.q[selected].votes.length / totalVotes) * 100) / 100}%.
+                </b>
+              </p>
+              <Button variant="light" className="mt-5">
                 <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
                   {' '}
                   ⬅️ Return to dashboard{' '}
