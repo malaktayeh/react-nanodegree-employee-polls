b9b70f4f5d2ec6eb11ac289a39722b8f7781255d
tenshi <malaktayeh@outlook.com>
2022-08-20 20:25:27 -0400

    my polls view

--- employee-polls/src/app/views/MyPolls.js
+++ employee-polls/src/app/views/MyPolls.js
@@ -1,17 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/esm/Container';
+import { useSelector } from 'react-redux';
+import Table from 'react-bootstrap/Table';
 import NavBar from '../components/Navbar';
-import Poll from '../components/Poll';
+import { questionsSelector } from '../features/questionsSlice';
+import { authedUserSelector } from '../features/authedUserSlice';
 
 function MyPolls() {
+  const { questions } = useSelector(questionsSelector);
+  const { authedUser } = useSelector(authedUserSelector);
+  const authedUserQuestions = Object.keys(questions)
+    .filter((q) => questions[q].author === authedUser.id)
+    .map((id) => questions[id]);
+
   return (
     <>
       <NavBar />
       <Container>
-        <h2 className="my-5 mx-2">Poll page</h2>
-        <Poll />
-        <Poll />
-        <Poll />
+        <h2 className="my-5 mx-2">Polls you have posted!</h2>
+        {authedUserQuestions.length === 0 ? (
+          <p>
+            You have not submitted any polls yet. Click <Link to="/add-poll">here</Link> to add one!
+          </p>
+        ) : (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Question No</th>
+                <th>Option 1</th>
+                <th>Option 2</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.values(authedUserQuestions).map((poll, index) => (
+                <tr key={poll.id}>
+                  <td>{index + 1}</td>
+                  <td>{poll.optionOne.text}</td>
+                  <td>{poll.optionTwo.text}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
       </Container>
     </>
   );
