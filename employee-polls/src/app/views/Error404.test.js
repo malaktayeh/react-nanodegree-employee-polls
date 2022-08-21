9c1d0c73cd89aaf59501b0653840ce66b4ad91a4
tenshi <malaktayeh@outlook.com>
2022-08-21 16:17:49 -0400

    initial state test for 404 page

--- employee-polls/src/app/views/Error404.test.js
+++ employee-polls/src/app/views/Error404.test.js
@@ -2,10 +2,18 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
+import reducer from '../features/authedUserSlice';
 import store from '../store';
 import Error404 from './Error404';
 
 describe('404 Page', () => {
+  test('should return the initial state', () => {
+    expect(reducer(undefined, { type: undefined })).toEqual({
+      authedUser: {},
+      errors: false,
+      status: 'idle'
+    });
+  });
   it('will match snapshot', () => {
     const view = renderer
       .create(
@@ -16,6 +24,40 @@ describe('404 Page', () => {
         </MemoryRouter>
       )
       .toJSON();
-    expect(view).toMatchSnapshot();
+    expect(view).toMatchInlineSnapshot(`
+      <div
+        className="mt-5 mb-2 container"
+      >
+        <div
+          className="mt-5"
+        >
+          <h2
+            className="mt-3"
+          >
+             4 0 4 !
+          </h2>
+        </div>
+        <button
+          className="mt-5 btn btn-light"
+          disabled={false}
+          type="button"
+        >
+          <a
+            href="/"
+            onClick={[Function]}
+            style={
+              Object {
+                "color": "black",
+                "textDecoration": "none",
+              }
+            }
+          >
+             
+            ⬅️ Return
+             
+          </a>
+        </button>
+      </div>
+    `);
   });
 });
